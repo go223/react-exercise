b147d8084fbfb0ac69f6e0d7b2cd8b24784e652f
huangbin <huangbin@showingdata.cn>
2018-10-13 22:01:56 +0800

    layout

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'normalize.css';
 
+// 获取图片相关的数据
+import imageD from './data/imageDatas.json';
+
+// 利用自执行函数，将图片信息转换成图片URL路径信息
+let imageDatas = (function genImageURL (imageDatasArr) {
+  for (let i = 0, len = imageDatasArr.length; i < len; i++) {
+    let singleImageData = imageDatasArr[i];
+    singleImageData.imageURL = require('./images/' + singleImageData.fileName);
+    imageDatasArr[i] = singleImageData;
+  }
+  return imageDatasArr;
+})(imageD);
+console.log(imageDatas)
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <section className="stage">
+        <section className="img-sec">1</section>
+        <nav className="controller-nav"></nav>
+      </section>
     );
   }
 }
